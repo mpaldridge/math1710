b2bc672391b1ac4c347ed6ec01b820da14bccb91
Matthew Aldridge <mpaldridge@gmail.com>
2022-10-30 20:44:24 +0000

    R5 data

--- R/produce5.R
+++ R/produce5.R
@@ -49,6 +49,6 @@ for(i in 0:99) {
   if (i < 10) filename <- paste("~/Documents/GitHub/math1710/docs/data/R5-0", i, ".csv", sep = "")
   else        filename <- paste("~/Documents/GitHub/math1710/docs/data/R5-",  i, ".csv", sep = "")
   
-  write.csv(data, filename)
+  write.csv(data, filename, row.names = FALSE)
 }
 
